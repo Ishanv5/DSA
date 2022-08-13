cc0f415f1553e927f3dca3584de70f8cfdeca9bb
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-08-14 01:18:24 +0530

    Add files via upload

--- ParanthesisMatch.c
+++ ParanthesisMatch.c
@@ -1,97 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-struct stack
-{
-    int size;
-    int top;
-    char *arr;
-};
- 
-int isEmpty(struct stack *ptr)
-{
-    if (ptr->top == -1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define Max 10
+int top=-1;
+int st[Max];
+void push(char c){
+    if(top==(Max-1))
+    printf("\n Stack overflow");
+else{
+   top = top + 1;
+   st[top] = c;
 }
- 
-int isFull(struct stack *ptr)
-{
-    if (ptr->top == ptr->size - 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
 }
- 
-void push(struct stack* ptr, char val){
-    if(isFull(ptr)){
-        printf("Stack Overflow! Cannot push %d to the stack\n", val);
-    }
-    else{
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
-    }
+char pop(char c){
+    if(top==-1)
+    printf("\n Stack Underflow");
+    else 
+    return (st[top--]);
 }
- 
-char pop(struct stack* ptr){
-    if(isEmpty(ptr)){
-        printf("Stack Underflow! Cannot pop from the stack\n");
-        return -1;
-    }
-    else{
-        char val = ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
-    }
-}
- 
-int parenthesisMatch(char * exp){
-    // Create and initialize the stack
-    struct stack* sp;
-    sp->size = 100;
-    sp->top = -1;
-    sp->arr = (char *)malloc(sp->size * sizeof(char));
- 
- 
-    for (int i = 0; exp[i]!='\0'; i++)
-    {
-        if(exp[i]=='('){
-            push(sp, '(');
-        }
-        else if(exp[i]==')'){
-            if(isEmpty(sp)){
-                return 0;
-            }
-            pop(sp); 
-        }
-    }
- 
-    if(isEmpty(sp)){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+void main(){
+ char exp[Max],temp;
+ int i,flag=1;
+ printf("Enter an Expreesion : ");
+ gets(exp);
+ for(i=0;i<strlen(exp);i++){
+    if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
+    push(exp[i]); 
+    if(exp[i]==')' || exp[i]=='}' || exp[i]==']')
+      if(top==-1)  flag=0;
+      else{
+         temp=pop(exp[i]);
+        if(exp[i]==')' && (temp=='{' || temp=='['))
+        flag=0;
+        if(exp[i]=='}' && (temp=='(' || temp=='['))
+        flag=0;
+        if(exp[i]==']' && (temp=='(' || temp=='{'))
+        flag=0;
+      }
+ }
+ if(top>=0)
+ flag=0;
+ if(flag==1)
+ printf("\n Valid expression");
+ else  
+ printf("\n Invalid expression");
     
-}
-int main()
-{
-    char * exp = "((8)(*--$$9))";
-    // Check if stack is empty
-    if(parenthesisMatch(exp)){
-        printf("The parenthesis is matching");
-    }
-    else{
-        printf("The parenthesis is not matching");
-    }
-    return 0;
 }

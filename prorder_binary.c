80f564ef416052e619866ec3c1d33398fcfe9731
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-08-27 18:36:10 +0530

    Add files via upload

---
+++ prorder_binary.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<malloc.h>
+
+struct node{
+    int data;
+    struct node* left;     // binary tree 1 left node
+    struct node* right;    // binary tree 1 right node
+};
+
+struct node* createNode(int data){
+    struct node *n; // creating a node pointer
+    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data; // Setting the data
+    n->left = NULL; // Setting the left and right children to NULL
+    n->right = NULL; // Setting the left and right children to NULL
+    // printf("%d \t ",n->data); 
+    return n; // Finally returning the created node
+    
+}
+void  preOrder(struct node * root){
+    if(root!=NULL){
+        printf("%d ",root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+};
+
+int main(){
+    
+   
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(2);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(4);
+    struct node *p3 = createNode(3);
+    struct node *p4 = createNode(6);
+
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    preOrder(p);
+    return 0;
+}
